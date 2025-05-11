417b613da358adf8829beb1aeebd24a3dd0ffe6f
seud <seudsahm1@gmail.com>
2025-05-11 16:59:20 +0300

    OnlineShop ListView Done

---
+++ app/src/main/java/com/example/online_shop/Activities/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.example.online_shop.Activities;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // to draw the layout on th entire screen. including the status bar and the navigation bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        ,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+}
---
+++ app/src/main/java/com/example/online_shop/Activities/IntroActivity.java
@@ -0,0 +1,22 @@
+package com.example.online_shop.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.online_shop.databinding.ActivityIntroBinding;
+
+public class IntroActivity extends BaseActivity {
+
+    ActivityIntroBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.startBtn.setOnClickListener(v->startActivity(
+            new Intent(IntroActivity.this, MainActivity.class)
+        ));
+    }
+}
